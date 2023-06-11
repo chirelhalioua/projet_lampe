4bcc6c99dcd78a9b32b83953b339cab1e9cbd48f
Chirel <chirelhalioua@gmail.com>
2023-06-11 12:25:43 +0200

    chirel le 11/06/2023

---
+++ app/src/main/java/com/example/projetlampe/Fonctionnalites.java
@@ -0,0 +1,19 @@
+package com.example.projetlampe;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Fonctionnalites extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fonctionnalite);
+    }
+
+}
